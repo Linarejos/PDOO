2e688ba1ab2f238e0e1a25e21e362bf959fae480
Elena Cantero <elenacantero@correo.ugr.es>
2019-09-27 18:10:14 +0200

    Empezando P2

--- Civitas-Java/Civitas/src/main/java/civitas/Casilla.java
+++ Civitas-Java/Civitas/src/main/java/civitas/Casilla.java
@@ -11,12 +11,72 @@
 
 package civitas;
 
+import java.util.ArrayList;
+
 public class Casilla {
+    private static int carcel;
+    private float importe;
     private String nombre;
     
     Casilla(String nombre){
         this.nombre = nombre;
     }
     
-    String getNombre(){ return nombre; }
+    Casilla(TituloPropiedad titulo){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    Casilla(float cantidad, String nombre){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    Casilla(int numCasillaCarcel, String nombre){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    Casilla(MazoSorpresas mazo, String nombre){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    public String getNombre(){ return nombre; }
+    
+    TituloPropiedad getTituloPropiedad(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private void informe(int iactual, ArrayList<Jugador> todos){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private void init(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    public boolean jugadorCorrecto(int iactual, ArrayList<Jugador> todos){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    void recibeJugador(int iactual, ArrayList<Jugador> todos){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private void recibeJugador_calle(int iactual, ArrayList<Jugador> todos){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private void recibeJugador_impuesto(int iactual, ArrayList<Jugador> todos){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private void recibeJugador_juez(int iactual, ArrayList<Jugador> todos){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private void recibeJugador_sorpresa(int iactual, ArrayList<Jugador> todos){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    public String toString(){
+        throw new UnsupportedOperationException("No implementado");
+    }
 }
---
+++ Civitas-Java/Civitas/src/main/java/civitas/TituloPropiedad.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civitas;
+
+/**
+ * @author elena
+ * @date 27/09/2019
+ */
+
+public class TituloPropiedad {    
+    private static float factorInteresesHipoteca = (float)1.1;
+    private float alquilerBase;
+    private float factorRevalorizacion;
+    private float hipotecaBase;
+    private boolean hipotecado;
+    private String nombre;
+    private int numCasas;
+    private int numHoteles;
+    private float precioCompra;
+    private float precioEdificar;
+    
+    TituloPropiedad(String nom, float ab, float fr, float hb, float pc,float pe){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    void actualizaPropietarioPorConversion(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    boolean cancelarHipoteca(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    int cantidadCasasHoteles(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    boolean comprar(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    boolean construirCasa(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    boolean construirHotel(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    boolean derruirCasas(int n, Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private boolean esEsteElPropietario(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    public boolean getHipotecado(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    float getImporteCancelarHipoteca(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private float getImporteHipoteca(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    String getNombre(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    int getNumCasas(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    int getNumHoteles(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private float getPrecioAlquiler(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    float getPrecioCompra(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    float getPrecioEdificar(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private float getPrecioVenta(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    Jugador getPropietario(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    boolean hipotecar(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    private boolean propietarioEncarcelado(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    boolean tienePropietario(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    void tramitarAlquiler(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    boolean vender(Jugador jugador){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+    
+    public String toString(){
+        throw new UnsupportedOperationException("No implementado"); 
+    }
+}
