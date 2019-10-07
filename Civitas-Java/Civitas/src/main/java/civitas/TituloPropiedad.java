ef927767155b52c600b1f7ce164983e504374808
Elena Cantero <elenacantero@correo.ugr.es>
2019-10-07 12:46:47 +0200

    Practica 2 Java
    Terminada clase TituloPropiedad

--- Civitas-Java/Civitas/src/main/java/civitas/TituloPropiedad.java
+++ Civitas-Java/Civitas/src/main/java/civitas/TituloPropiedad.java
@@ -35,35 +35,96 @@ public class TituloPropiedad {
         this.numHoteles = 0;
         this.hipotecado = false;
     }
+         
+    float getImporteCancelarHipoteca(){
+        return getImporteHipoteca()*factorInteresesHipoteca;
+    }
+    
+    private float getPrecioAlquiler(){
+        float precioAlquiler = (float)(this.alquilerBase*(1+(this.numCasas*0.5)+(this.numHoteles*2.5)));
+        
+        //Si no tiene dinero, bancarrota y fin juego
+        
+        if(hipotecado || propietarioEncarcelado()){
+            precioAlquiler = 0;
+        }
+        
+        return precioAlquiler;
+    }
+    
+    private float getPrecioVenta(){
+        return getPrecioCompra() + this.getPrecioEdificar()*factorRevalorizacion;
+    }
+    
+    boolean cancelarHipoteca(Jugador jugador){
+        if(!getHipotecado() && esEsteElPropietario(jugador)){
+            jugador.paga(getImporteCancelarHipoteca());
+            hipotecado = false;
+            return true;
+        }        
+        return false;
+    }
+    
+    boolean hipotecar(Jugador jugador){
+        if(!getHipotecado() && esEsteElPropietario(jugador)){
+            jugador.recibe(this.getImporteHipoteca());
+            hipotecado = true;
+            return true;
+        }
+        
+        return false;
+    }
+    
+    void tramitarAlquiler(Jugador jugador){
+        if(!getHipotecado() && !esEsteElPropietario(jugador)){
+            jugador.pagaAlquiler(this.getImporteHipoteca());
+            propietario.recibe(getImporteHipoteca());
+        }
+    }
+    
+    private boolean propietarioEncarcelado(){ return propietario.encarcelado; }
+    
+    int cantidadCasasHoteles(){ return numCasas + numHoteles; }
+    
+    boolean derruirCasas(int n, Jugador jugador){
+        if(esEsteElPropietario(jugador) && getNumCasas() >= n){
+            numCasas -= n;
+            return true;
+        }
+        
+        return false;
+    }   
+    
+    boolean construirCasa(Jugador jugador){
+        boolean construido = false;
+        
+        if(esEsteElPropietario(jugador)){
+            jugador.paga(getPrecioCompra());
+            numCasas++;
+            construido = true;
+        }
+        
+        return construido;
+    }
+    
+    boolean construirHotel(Jugador jugador){
+        boolean construido = false;
+        
+        if(esEsteElPropietario(jugador)){
+            jugador.paga(getPrecioCompra());
+            numHoteles++;
+            construido = true;
+        }
+        
+        return construido;
+    }
+    
+    boolean comprar(Jugador jugador){ return propietario == null; }
     
     void actualizaPropietarioPorConversion(Jugador jugador){
         throw new UnsupportedOperationException("No implementado"); 
     }
-    
-    boolean cancelarHipoteca(Jugador jugador){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
-    int cantidadCasasHoteles(){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
-    boolean comprar(Jugador jugador){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
-    boolean construirCasa(Jugador jugador){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
-    boolean construirHotel(Jugador jugador){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
-    boolean derruirCasas(int n, Jugador jugador){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
+       
     private boolean esEsteElPropietario(Jugador jugador){
         throw new UnsupportedOperationException("No implementado"); 
     }
@@ -71,11 +132,7 @@ public class TituloPropiedad {
     public boolean getHipotecado(){
         throw new UnsupportedOperationException("No implementado"); 
     }
-    
-    float getImporteCancelarHipoteca(){
-        return getImporteHipoteca()*factorInteresesHipoteca;
-    }
-    
+        
     private float getImporteHipoteca(){
         throw new UnsupportedOperationException("No implementado"); 
     }
@@ -92,18 +149,6 @@ public class TituloPropiedad {
         throw new UnsupportedOperationException("No implementado"); 
     }
     
-    private float getPrecioAlquiler(){
-        float precioAlquiler = (float)(this.alquilerBase*(1+(this.numCasas*0.5)+(this.numHoteles*2.5)));
-        
-        //Si no tiene dinero, bancarrota y fin juego
-        
-        if(hipotecado || propietarioEncarcelado()){
-            precioAlquiler = 0;
-        }
-        
-        return precioAlquiler;
-    }
-    
     float getPrecioCompra(){
         throw new UnsupportedOperationException("No implementado"); 
     }
@@ -111,31 +156,15 @@ public class TituloPropiedad {
     float getPrecioEdificar(){
         throw new UnsupportedOperationException("No implementado"); 
     }
-    
-    private float getPrecioVenta(){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
+            
     Jugador getPropietario(){
         throw new UnsupportedOperationException("No implementado"); 
-    }
-    
-    boolean hipotecar(Jugador jugador){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
-    private boolean propietarioEncarcelado(){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
+    }   
     
     boolean tienePropietario(){
         throw new UnsupportedOperationException("No implementado"); 
     }
-    
-    void tramitarAlquiler(Jugador jugador){
-        throw new UnsupportedOperationException("No implementado"); 
-    }
-    
+           
     boolean vender(Jugador jugador){
         throw new UnsupportedOperationException("No implementado"); 
     }
