54d6ee1e2572a6437de7431e5e53fab6df692df5
Elena Cantero <elenacantero@correo.ugr.es>
2019-10-07 13:02:32 +0200

    Practica 2 Java
    Haciendo clase Sorpresa

--- Civitas-Java/Civitas/src/main/java/civitas/Sorpresa.java
+++ Civitas-Java/Civitas/src/main/java/civitas/Sorpresa.java
@@ -8,8 +8,8 @@ package civitas;
 import java.util.ArrayList;
 
 /**
- *
  * @author elena
+ * @date 07/10/2019
  */
 public class Sorpresa {
     private String texto;
@@ -17,7 +17,50 @@ public class Sorpresa {
     private MazoSorpresas mazo;
     private TipoSorpresa tipo;
     private Tablero tablero;
-            
+    
+    Sorpresa(TipoSorpresa tipo, Tablero tablero){ //Construir la sorpresa que envía la cárcel
+        init();
+        this.tipo = tipo;
+        this.tablero = tablero;
+        this.texto = "De camino a la carcel";
+    }
+    
+    Sorpresa(TipoSorpresa tipo, Tablero tablero, int valor, String texto){  //Sorpresa que envía el jugador a otra casilla
+        init();
+        this.tipo = tipo;
+        this.tablero = tablero;
+        this.texto = texto;
+        this.valor = valor;
+    }
+    
+    Sorpresa(TipoSorpresa tipo, int valor, String texto){   //Resto de sorpresas
+        init();
+        this.tipo = tipo;
+        this.texto = texto;
+        this.valor = valor;
+    }
+    
+    Sorpresa(TipoSorpresa tipo, MazoSorpresas mazo){    //Sorpresa para evitar la cárcel
+        init();
+        this.tipo = tipo;
+        this.texto = "Evitando la carcel";
+        this.mazo = mazo;
+    }
+    
+    private void init(){ 
+        this.valor = -1;
+        this.mazo = null;
+        this.tablero = null;
+    }
+
+    public boolean jugadorCorrecto(int actual, ArrayList<Jugador> todos){
+        return todos.size() > actual;
+    }
+    
+    private void informe(int actual, ArrayList<Jugador> todos){
+        
+    }
+    
     void aplicarAJugador(int actual, ArrayList<Jugador> todos){ throw new UnsupportedOperationException("No implementado"); }    
     private void aplicarAJugador_irACasilla(int actual, ArrayList<Jugador> todos){ throw new UnsupportedOperationException("No implementado"); }
     private void aplicarAJugador_irCarcel(int actual, ArrayList<Jugador> todos){ throw new UnsupportedOperationException("No implementado"); }    
@@ -25,17 +68,9 @@ public class Sorpresa {
     private void aplicarAJugador_porCasaHotel(int actual, ArrayList<Jugador> todos){ throw new UnsupportedOperationException("No implementado"); }    
     private void aplicarAJugador_porJugador(int actual, ArrayList<Jugador> todos){ throw new UnsupportedOperationException("No implementado"); }    
     private void aplicarAJugador_salirCarcel(int actual, ArrayList<Jugador> todos){ throw new UnsupportedOperationException("No implementado"); }    
-    private void informe(int actual, ArrayList<Jugador> todos){ throw new UnsupportedOperationException("No implementado"); }    
-    private void init(){ throw new UnsupportedOperationException("No implementado"); }
-    
-    public boolean jugadorCorrecto(int actual, ArrayList<Jugador> todos){ throw new UnsupportedOperationException("No implementado"); }
+       
     void salirDelMazo(){ throw new UnsupportedOperationException("No implementado"); }
-    
-    Sorpresa(TipoSorpresa tipo, Tablero tablero){ throw new UnsupportedOperationException("No implementado"); }
-    Sorpresa(TipoSorpresa tipo, Tablero tablero, int valor, String texto){ throw new UnsupportedOperationException("No implementado"); }
-    Sorpresa(TipoSorpresa tipo, int valor, String texto){ throw new UnsupportedOperationException("No implementado"); }
-    Sorpresa(TipoSorpresa tipo, MazoSorpresas mazo){ throw new UnsupportedOperationException("No implementado"); }
-
+        
     @Override
     public String toString(){ throw new UnsupportedOperationException("No implementado"); }
     
